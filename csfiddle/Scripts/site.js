779ad6e95262153600fd67bfaf9043f0135ec746
Matt Davies <matt@mdavies.net>
2013-08-10 00:17:51 +0800

    Starting on the mountain of refactoring ahead

--- csfiddle/Scripts/site.js
+++ csfiddle/Scripts/site.js
@@ -1,4 +1,33 @@
 $(document).ready(function () {
+    csfiddle = {
+        editor: CodeMirror.fromTextArea(document.getElementById('InputCode'), {
+            lineNumbers: true,
+            matchBrackets: true,
+            mode: "text/x-csharp",
+            indentUnit: 4,
+            autofocus: true
+        }),
+
+        runCode: function() {
+            $("#run").html("Running...");
+            $.post('/Run', { InputCode: csfiddle.editor.getValue() }, function(data) {
+                $('#result').html("<pre>" + data + "</pre>");
+            })
+                .always(function () { $("#run").html("Run"); })
+                .fail(function () { $("#result").html("<pre>Failed to run.</pre>"); });
+        },
+
+        saveCode: function () {
+            $("#save").html("Saving...");
+            $.post('/Save', { Id: $("#id").val(), InputCode: csfiddle.editor.getValue() }, function(id) {
+                if ($("#id").val() != id) {
+                    history.pushState(null, "Viewing Fiddle " + id, "/" + id);
+                    $("#id").val(id);
+                }
+            }).always(function() { $("#save").html("Save"); });
+        }
+    };
+    
     $('body').layout({
         resizeWhileDragging: true,
         sizable: false,
@@ -13,13 +42,7 @@ $(document).ready(function () {
         closable: false
     });
 
-    csfiddle = {
-        editor: CodeMirror.fromTextArea(document.getElementById('InputCode'), {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "text/x-csharp",
-            indentUnit: 4,
-            autofocus: true
-        })
-    };
+    $("#new").click(function () { window.location = ""; });
+    $("#run").click(csfiddle.runCode);
+    $("#save").click(csfiddle.saveCode);
 });
